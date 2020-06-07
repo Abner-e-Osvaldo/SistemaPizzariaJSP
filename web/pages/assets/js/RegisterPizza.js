bf9b12d7336b4d1b49c80554d7b8763add8f9d7d
Abner Matheus <abnerm80@gmail.com>
2020-06-06 23:57:43 -0300

    Alterando Pizza e Listando

--- web/pages/assets/js/RegisterPizza.js
+++ web/pages/assets/js/RegisterPizza.js
@@ -10,6 +10,45 @@ $(document).ready(function(){
   modal.find('.modal-body input').val(recipient)
 })
 
+
+  $('#exampleModal2').on('show.bs.modal', function (event) {
+      
+      
+  var button = $(event.relatedTarget);
+  
+  const idPizza = button[0].parentElement.parentElement.getAttribute('value');
+  const nome = button[0].parentElement.parentElement.querySelectorAll('td')[0].innerText;
+  const descricao = button[0].parentElement.parentElement.querySelectorAll('td')[1].innerText; 
+  const tamanho = button[0].parentElement.parentElement.querySelectorAll('td')[2].innerText;
+  const valorBase = button[0].parentElement.parentElement.querySelectorAll('td')[3].innerText;
+  const acrescimoTamanho = button[0].parentElement.parentElement.querySelectorAll('td')[4].innerText;
+  
+   var recipient = button.data('whatever') // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this)
+  
+  modal.find('.modal-body input').val(recipient);
+  
+  $("[name=nomePizzaEdit]").val(nome);
+  $("[name=descricaoPizzaEdit]").val(descricao);
+  $("[name=valorPizzaEdit]").val(valorBase);
+  $("[name=acrescimoPizzaEdit]").val(acrescimoTamanho);
+  $("[name=idPizza]").val(idPizza);
+
+  
+ 
+ /* document.getElementsByName('descricaoPizzaEdit')[0].value=descricao;
+  document.getElementsByName('valorPizzaEdit')[0].value=valorBase;
+  document.getElementsByName('acrescimoPizzaEdit')[0].value=acrescimoTamanho;
+  */
+  
+  
+  
+  // Button that triggered the modal
+ 
+})
+
     
     
     
